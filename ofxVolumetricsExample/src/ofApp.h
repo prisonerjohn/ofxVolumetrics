050227399931d9952b571ffad434cb0dff87b3b7
Elie Zananiri <prisonerjohn@gmail.com>
2016-04-01 13:32:10 -0400

    Updated example.

--- ofxVolumetricsExample/src/ofApp.h
+++ ofxVolumetricsExample/src/ofApp.h
@@ -1,33 +1,44 @@
 #pragma once
 
+//#define USE_VOLUMETRICS_3D 1
+
 #include "ofMain.h"
-#include "ofxVolumetrics.h"
+#include "ofxImageSequencePlayer.h"
+
+#ifdef USE_VOLUMETRICS_3D
+#include "ofxVolumetrics3D.h"
+#else
+#include "ofxVolumetricsArray.h"
+#endif
 
 class ofApp : public ofBaseApp
 {
-
 public:
-    void setup();
-    void update();
-    void draw();
+	void setup();
+	void update();
+	void draw();
 
-    void keyPressed  (int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 
-    ofEasyCam cam;
-    
-    ofxVolumetrics myVolume;
-    unsigned char * volumeData;
-    int volWidth, volHeight, volDepth;
-    ofImage background;
-    ofxImageSequencePlayer imageSequence;
-    bool linearFilter;
+	ofEasyCam cam;
 
+#ifdef USE_VOLUMETRICS_3D
+	ofxVolumetrics3D myVolume;
+#else
+	ofxVolumetricsArray myVolume;
+#endif
+
+	unsigned char * volumeData;
+	int volWidth, volHeight, volDepth;
+	ofImage background;
+	ofxImageSequencePlayer imageSequence;
+	bool linearFilter;
 };
